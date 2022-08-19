d92a3ac42ff5558783f25618b3270ed61350761a
Xsufu <aluno.macarrao@gmail.com>
2022-08-19 21:59:34 +0700

    Реализован запуск экрана игры по нажатию на кнопку NewGame

--- app/src/main/java/com/xolary/tetris/MainActivity.kt
+++ app/src/main/java/com/xolary/tetris/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.xolary.tetris
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 
@@ -19,6 +21,13 @@ class MainActivity : AppCompatActivity() {
         val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
         val btnExit = findViewById<Button>(R.id.btn_exit)
         tvHighScore = findViewById(R.id.tv_high_score)
+
+        btnNewGame.setOnClickListener(this::onBtnNewGameClick)
+    }
+
+    private fun onBtnNewGameClick(view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
     }
 
 }
