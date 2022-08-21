cf1e28009705c6285e8048532fc3907245fe8678
Xsufu <aluno.macarrao@gmail.com>
2022-08-21 21:34:35 +0700

    Добавлены:  Свойства класса:    - Текущий счёт    - Экземпляр доступа к AppPreferences    - Текущий игровой блок    - Текущий статус игры    - Массив игрового поля
     Геттеры и сеттеры:
       - Установка AppPreferences
       - Получение состояния ячейки в указаной позиции
       - Установка состояния имеющейся ячейке

--- app/src/main/java/com/xolary/tetris/models/AppModel.kt
+++ app/src/main/java/com/xolary/tetris/models/AppModel.kt
@@ -6,11 +6,43 @@ import com.xolary.tetris.helper.array2dOfByte
 import com.xolary.tetris.storage.AppPreferences
 
 class AppModel {
+    // Текущий счёт
+    var score: Int = 0
+    // Доступ к SharedPreferences
+    private var preferences: AppPreferences? = null
+
+    // Текущий игровой блок
+    var currentBlock: Block? = null
+    // Текущее состояние игры
+    var currentState: String = Statuses.AWAITING_START.name
+
+    // Двумерный массив - игровое поле
+    private var field: Array<ByteArray> = array2dOfByte(
+        FieldConstants.ROW_COUNT.value,
+        FieldConstants.COLUMN_COUNT.value
+    )
+
+    // Состояния статуса
     enum class Statuses {
-        AWAITING_RESTART, ACTIVE, INACTIVE, OVER
+        AWAITING_START, ACTIVE, INACTIVE, OVER
     }
 
+    // Состояния движения
     enum class Motions {
         LEFT, RIGHT, DOWN, ROTATE
+    }
+
+    fun setPreferences(preferences: AppPreferences?) {
+        this.preferences = preferences
+    }
+
+    fun getCellStatus(row: Int, column: Int): Byte? {
+        return field[row][column]
+    }
+
+    private fun setCellStatus(row: Int, column: Int, status: Byte?) {
+        if (status != null) {
+            field[row][column] = status
+        }
     }
 }
