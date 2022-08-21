3ae98fd31eed13c3856c8c7714470956dc686ee1
Xsufu <aluno.macarrao@gmail.com>
2022-08-21 20:48:16 +0700

    Создан сеттер и функция для создания блоков

--- app/src/main/java/com/xolary/tetris/models/Block.java
+++ app/src/main/java/com/xolary/tetris/models/Block.java
@@ -3,11 +3,38 @@ package com.xolary.tetris.models;
 import android.graphics.Color;
 import android.graphics.Point;
 
+import java.util.Random;
+
 public class Block {
     private int shapeIndex;
     private int frameNumber;
     private BlockColor color;
     private Point position;
+
+    private Block(int shapeIndex, BlockColor blockColor) {
+        this.frameNumber = 0;
+        this.shapeIndex = shapeIndex;
+        this.color = blockColor;
+        this.position = new Point(FieldConstants.COLUMN_COUNT.getValue() / 2, 0);
+    }
+
+    // Функция создания блока
+    public static Block createBlock() {
+        // Экземпляр класс Random
+        Random random = new Random();
+        // Выбираем случайную форму из списка
+        int shapeIndex = random.nextInt(Shape.values().length);
+        // Выбираем случайный цвет для блока
+        BlockColor blockColor = BlockColor.values()
+                [random.nextInt(BlockColor.values().length)];
+
+        // Создаём блок с указанными параметрами
+        Block block = new Block(shapeIndex, blockColor);
+        // Задаём его стартовую позицию
+        block.position.x = block.position.x - Shape.values()
+                [shapeIndex].getStartPosition();
+        return block;
+    }
 
     public enum BlockColor {
         PINK(Color.rgb(255, 105, 180), (byte) 2),
