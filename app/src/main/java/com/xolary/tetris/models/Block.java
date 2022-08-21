d4282a96b2962765b80180d9dbc2d1c631694c07
Xsufu <aluno.macarrao@gmail.com>
2022-08-21 20:58:41 +0700

    Созданы геттеры и сеттеры для важных свойств

--- app/src/main/java/com/xolary/tetris/models/Block.java
+++ app/src/main/java/com/xolary/tetris/models/Block.java
@@ -3,6 +3,8 @@ package com.xolary.tetris.models;
 import android.graphics.Color;
 import android.graphics.Point;
 
+import androidx.annotation.NonNull;
+
 import java.util.Random;
 
 public class Block {
@@ -16,6 +18,45 @@ public class Block {
         this.shapeIndex = shapeIndex;
         this.color = blockColor;
         this.position = new Point(FieldConstants.COLUMN_COUNT.getValue() / 2, 0);
+    }
+
+    public static int getColor(byte value) {
+        for (BlockColor colour : BlockColor.values()) {
+            if (value == colour.byteValue) {
+                return colour.rgbValue;
+            }
+        }
+        return -1;
+    }
+
+    public final void setState(int frame, Point position) {
+        this.frameNumber = frame;
+        this.position = position;
+    }
+
+    @NonNull
+    public final byte[][] getShape(int frameNumber) {
+        return Shape.values()[shapeIndex].getFrame(frameNumber).as2dByteArray();
+    }
+
+    public Point getPosition() {
+        return this.position;
+    }
+
+    public final int getFrameCount() {
+        return Shape.values()[shapeIndex].getFrameCount();
+    }
+
+    public int getFrameNumber() {
+        return frameNumber;
+    }
+
+    public int getColor() {
+        return color.rgbValue;
+    }
+
+    public byte getStaticValue() {
+        return color.byteValue;
     }
 
     // Функция создания блока
