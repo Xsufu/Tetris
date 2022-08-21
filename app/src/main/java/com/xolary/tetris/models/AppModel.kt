a5f292a5c6695c710532e8ff864169197bc39da5
Xsufu <aluno.macarrao@gmail.com>
2022-08-22 02:25:17 +0700

    Добавлены методы:   - Обновления поля   - Проверки на наличе места на поле   - Сканирования строк на заполненность ячеек   - Очищения и сдвига строки   - Сохранения состояния ячеек   - Фиксации блока в поле   - Сброса поля   - Старта, рестарта и конца игры   - Сброса модели

--- app/src/main/java/com/xolary/tetris/models/AppModel.kt
+++ app/src/main/java/com/xolary/tetris/models/AppModel.kt
@@ -73,6 +73,7 @@ class AppModel {
         currentBlock = Block.createBlock()
     }
 
+    // Проверка допустимости движения
     private fun validTransaction(position: Point, shape: Array<ByteArray>): Boolean {
         if (position.y < 0 || position.x < 0) {
             return false
@@ -86,12 +87,188 @@ class AppModel {
                     val y = position.y + i
                     val x = position.x + j
                     if (CellConstants.EMPTY.value != shape[i][j] &&
-                        CellConstants.EMPTY.value != field[y][x]) {
+                        CellConstants.EMPTY.value != field[y][x]
+                    ) {
                         return false
                     }
                 }
             }
         }
         return true
+    }
+
+    // Перемещение разрешённого блока
+    private fun moveValid(position: Point, frameNumber: Int?): Boolean {
+        val shape: Array<ByteArray>? = currentBlock?.getShape(frameNumber as Int)
+        return validTransaction(position, shape as Array<ByteArray>)
+    }
+
+    // Метод генерирует обновление поля
+    fun generateField(action: String) {
+        if (isGameActive()) {
+            resetField()
+            var frameNumber: Int? = currentBlock?.frameNumber
+            val coordinate: Point? = Point()
+            coordinate?.x = currentBlock?.position?.x
+            coordinate?.y = currentBlock?.position?.y
+
+            when (action) {
+                Motions.LEFT.name -> {
+                    coordinate?.x = currentBlock?.position?.x?.minus(1)
+                }
+
+                Motions.RIGHT.name -> {
+                    coordinate?.x = currentBlock?.position?.x?.plus(1)
+                }
+
+                Motions.DOWN.name -> {
+                    coordinate?.y = currentBlock?.position?.y?.plus(1)
+                }
+
+                Motions.ROTATE.name -> {
+                    frameNumber = frameNumber?.plus(1)
+                    if (frameNumber != null) {
+                        if (frameNumber >= currentBlock?.frameCount as Int) {
+                            frameNumber = 0
+                        }
+                    }
+                }
+            }
+
+            if (!moveValid(coordinate as Point, frameNumber)) {
+                translateBlock(
+                    currentBlock?.position as Point,
+                    currentBlock?.frameNumber as Int
+                )
+                if (Motions.DOWN.name == action) {
+                    boostScore()
+                    persistCellData()
+                    assessField()
+                    generateNextBlock()
+                    if (!blockAdditionPossible()) {
+                        currentState = Statuses.OVER.name
+                        currentBlock = null
+                        resetField(false)
+                    }
+                }
+            } else {
+                if (frameNumber != null) {
+                    translateBlock(coordinate, frameNumber)
+                    currentBlock?.setState(frameNumber, coordinate)
+                }
+            }
+        }
+    }
+
+    // Проверка на незаполненность поля и возможности перемещения блока
+    private fun blockAdditionPossible(): Boolean {
+        if (!moveValid(
+                currentBlock?.position as Point,
+                currentBlock?.frameNumber
+            )
+        ) {
+            return false
+        }
+        return true
+    }
+
+    // Сканирование строк на заполненность ячеек
+    private fun assessField() {
+        for (i in 0 until field.size) {
+            var emptyCells = 0
+            for (j in 0 until field[i].size) {
+                val status = getCellStatus(i, j)
+                val isEmpty = CellConstants.EMPTY.value == status
+                if (isEmpty)
+                    emptyCells++
+                if (emptyCells == 0)
+                    shiftRows(i)
+            }
+        }
+    }
+
+    // Очищение строки в случае полного заполнения
+    private fun shiftRows(nToRow: Int) {
+        if (nToRow > 0) {
+            for (j in nToRow - 1 downTo 0) {
+                for (m in 0 until field[j].size) {
+                    setCellStatus(j + 1, m, getCellStatus(j, m))
+                }
+            }
+        }
+
+        for (j in 0 until field[0].size) {
+            setCellStatus(0, j, CellConstants.EMPTY.value)
+        }
+    }
+
+    // Сохранение состояния всех ячеек
+    private fun persistCellData() {
+        for (i in 0 until field.size) {
+            for (j in 0 until field[i].size) {
+                var status = getCellStatus(i, j)
+                if (status == CellConstants.EPHEMERAL.value) {
+                    status = currentBlock?.staticValue
+                    setCellStatus(i, j, status)
+                }
+            }
+        }
+    }
+
+    // Фиксация блока в поле
+    private fun translateBlock(position: Point, frameNumber: Int) {
+        synchronized(field) {
+            val shape: Array<ByteArray>? = currentBlock?.getShape(frameNumber)
+            if (shape != null) {
+                for (i in shape.indices) {
+                    for (j in 0 until shape[i].size) {
+                        val y = position.y + i
+                        val x = position.x + j
+                        if (CellConstants.EMPTY.value != shape[i][j]) {
+                            field[y][x] = shape[i][j]
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Сброс поля
+    private fun resetField(ephemeralCellsOnly: Boolean = true) {
+        for (i in 0 until FieldConstants.ROW_COUNT.value) {
+            (0 until FieldConstants.COLUMN_COUNT.value)
+                .filter {
+                    !ephemeralCellsOnly || field[i][it] ==
+                            CellConstants.EPHEMERAL.value
+                }
+                .forEach { field[i][it] = CellConstants.EMPTY.value }
+        }
+    }
+
+    // Старт игры
+    fun startGame() {
+        if (!isGameActive()) {
+            currentState = Statuses.ACTIVE.name
+            generateNextBlock()
+        }
+    }
+
+    // Рестарт игры
+    fun restartGame() {
+        resetModel()
+        startGame()
+    }
+
+    // Конец игры
+    fun endGame() {
+        score = 0
+        currentState = Statuses.OVER.name
+    }
+
+    // Сборс модели
+    private fun resetModel() {
+        resetField(false)
+        currentState = Statuses.AWAITING_START.name
+        score = 0
     }
 }
